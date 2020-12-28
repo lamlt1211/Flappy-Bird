22d3a532daadeb40e6153963559f8a1b5224f1b8
TungLam <lamchuot0@gmail.com>
2020-12-28 22:31:38 +0700

    First commit

---
+++ Assets/scripts/Parallaxer.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxer : MonoBehaviour
+{
+    class PollObject
+    {
+        public Transform transform;
+        public bool inUse;
+        public PollObject(Transform t) { transform = t; }
+        public void Use() { inUse = true; }
+        public void Dispose() { inUse = false; }
+    }
+
+    [System.Serializable]
+    public struct YSpawnRange
+    {
+        public float min;
+        public float max;
+    }
+
+    public GameObject Prefab;
+    public int poolSize;
+    public float shiftSpeed;
+    public float spawnRate;
+
+    public YSpawnRange ySpawnRange;
+    public Vector3 defaultSpawnPos;
+    public bool spawnImmediate;
+    public Vector3 immediateSpawnPos;
+    public Vector2 targetAspectRatio;
+
+    float spawnTimer;
+    float targetAspect;
+    PollObject[] pollObjects;
+    GameManager game;
+
+     void Awake()
+    {
+        Configure();
+
+    }
+     void Start()
+    {
+        game = GameManager.Instance;
+    }
+
+     void OnEnable()
+    {
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+    void OnDisable()
+    {
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameOverConfirmed()
+    {
+        for (int i = 0; i < pollObjects.Length; i++)
+        {
+            pollObjects[i].Dispose();
+            pollObjects[i].transform.position = Vector3.one * 1000; 
+        }
+        if (spawnImmediate)
+        {
+            SpawnImmediate();
+        }
+    }
+
+     void Update()
+    {
+        if (game.GameOver) return;
+
+        Shift();
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer > spawnRate)
+        {
+            Spawn();
+            spawnTimer = 0;
+        }
+       
+    }
+
+    void Configure()
+    {
+        targetAspect = targetAspectRatio.x / targetAspectRatio.y;
+        pollObjects = new PollObject[poolSize];
+        for (int i = 0; i < pollObjects.Length; i++)
+        {
+            GameObject go = Instantiate(Prefab) as GameObject;
+            Transform t = go.transform;
+            t.SetParent(transform);
+            t.position = Vector3.one * 1000;
+            pollObjects[i] = new PollObject(t);
+        }
+
+        if (spawnImmediate)
+        {
+            SpawnImmediate();
+        }
+    }
+
+    void Spawn()
+    {
+        Transform t = GetPoolObject();
+        if (t == null) return;
+        Vector3 pos = Vector3.zero;
+        pos.x = (defaultSpawnPos.x * Camera.main.aspect) / targetAspect;
+        pos.y = Random.Range(ySpawnRange.min, ySpawnRange.max);
+        t.position = pos;
+    }
+
+    void SpawnImmediate()
+    {
+        Transform t = GetPoolObject();
+        if (t == null) return;
+        Vector3 pos = Vector3.zero;
+        pos.x = (immediateSpawnPos.x * Camera.main.aspect)/targetAspect;
+        pos.y = Random.Range(ySpawnRange.min, ySpawnRange.max);
+        t.position = pos;
+        Spawn();
+    }
+
+    void Shift()
+    {
+        for (int i = 0; i < pollObjects.Length; i++)
+        {
+            pollObjects[i].transform.localPosition += -Vector3.right * shiftSpeed * Time.deltaTime;
+            //pollObjects[i].transform.position += -Vector3.right * shiftSpeed * Time.deltaTime;
+            CheckDisposeObject(pollObjects[i]);
+        }
+    }
+
+    void CheckDisposeObject(PollObject pollObject)
+    {
+        if (pollObject.transform.position.x < (-defaultSpawnPos.x * Camera.main.aspect)/ targetAspect)
+        {
+            pollObject.Dispose();
+            pollObject.transform.position = Vector3.one * 1000;
+        }
+    }
+
+    Transform GetPoolObject()
+    {
+        for (int i = 0; i < pollObjects.Length; i++)
+        {
+            if (!pollObjects[i].inUse)
+            {
+                pollObjects[i].Use();
+                return pollObjects[i].transform;
+            }
+        }
+        return null;
+    }
+}
---
+++ Assets/scripts/TapController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour
+{
+
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    Rigidbody2D rigidbody;
+    Quaternion downRatation;
+    Quaternion forwardRotation;
+
+    GameManager game;
+
+     void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRatation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+        game = GameManager.Instance;
+        //rigidbody.simulated = false; // comment cho nay` 
+    }
+
+    void OnEnable()
+    {
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+     void OnDisable()
+    {
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameStarted()
+    {
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.simulated = true;
+    }
+
+    void OnGameOverConfirmed()
+    {
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity; 
+    }
+    void Update()
+    {
+        if (game.GameOver) return;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRatation, tiltSmooth * Time.deltaTime);
+    }
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ScoreZone"){
+            //register a score event
+            OnPlayerScored(); // event sent to GameNanager;
+            //play a sound
+        }
+        if (collision.gameObject.tag == "DeadZone")
+        {
+            rigidbody.simulated = false;
+            //register a dead event
+            OnPlayerDied();  // event sent to GameManager;
+            //play a sound
+        }
+    }
+}
